bd500986b0f5fb5dd9952063eb33a94ca4182488
swasti1918 <jnswasti07@gmail.com>
2024-07-18 12:45:14 +0530

    Add files via upload

---
+++ chapter-1/module.py
@@ -0,0 +1,11 @@
+import  pyjokes 
+
+# ("printing jokes..")print
+
+# This prints a random joke         ******** to comment out single line use ctrl+/
+joke= pyjokes.get_joke()
+print(joke)
+"""
+so thanks 
+that was my program             ********* to comment multi line use  """/'''
+"""
---
+++ chapter-1/pratiseset/problem01.py
@@ -0,0 +1,21 @@
+
+# Write a program to print Twinkle twinkle little star poem in python
+
+
+print('''
+      Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+
+      ''')
+
+      
